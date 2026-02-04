08c6b5e1b727e8f58f38444c78451e4dd432e7ef
moneythemoney999 <193152889+moneythemoney999@users.noreply.github.com>
2026-02-04 13:53:36 -0300

    Point de sauvegarde

--- lib/mugen_haiti.dart
+++ lib/mugen_haiti.dart
@@ -9,34 +9,60 @@ class MonJeu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // On retire toute complexité pour ce test.
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Mugen Haïti',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        // Fond gris pour s'assurer que l'on ne regarde pas un widget blanc sur fond blanc.
-        backgroundColor: Colors.grey[800],
-        body: Center(
-          // Centrer le personnage à l'écran.
-          child: Container(
-            width: 100,
-            height: 100,
-            decoration: const BoxDecoration(
-              color: Colors.red,
-              shape: BoxShape.circle,
-            ),
-            child: const Center(
-              child: Text(
-                'MH',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
+      home: PageJeu(),
+    );
+  }
+}
+
+// On re-transforme notre page en "StatefulWidget" pour pouvoir
+// sauvegarder et modifier la position du personnage.
+class PageJeu extends StatefulWidget {
+  const PageJeu({Key? key}) : super(key: key);
+
+  @override
+  State<PageJeu> createState() => _EtatPageJeu();
+}
+
+class _EtatPageJeu extends State<PageJeu> {
+  // On définit une position X et Y pour notre personnage.
+  // Pour l'instant, on met des valeurs fixes pour tester.
+  double _positionX = 150.0;
+  double _positionY = 250.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      // On utilise un "Stack" pour nous permettre de superposer des widgets.
+      // C'est parfait pour placer un personnage sur un fond.
+      body: Stack(
+        children: [
+          Positioned(
+            left: _positionX,
+            top: _positionY,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+              child: const Center(
+                child: Text(
+                  'MH',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
