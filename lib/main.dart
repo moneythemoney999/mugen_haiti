afc4bdfae9f552224da0c9333af326cabfa92650
moneythemoney999 <193152889+moneythemoney999@users.noreply.github.com>
2026-02-05 22:03:04 -0300

    Point de sauvegarde

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+// Ce fichier sert de point d'entrée standard pour Flutter.
+
+// On importe notre fichier de jeu principal et on le nomme "jeu_principal" pour éviter les conflits.
+import 'package:mugen_haiti/mugen_haiti.dart' as jeu_principal;
+
+void main() {
+  // On appelle la fonction main() qui se trouve dans lib/mugen_haiti.dart.
+  jeu_principal.main();
+}
